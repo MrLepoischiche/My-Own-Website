6155f815ffdf302e22e56a5bb2823fe94427cb97
Esteban JEAN <esteban-jean.pro@hotmail.com>
2023-08-07 11:45:36 +0200

    update game_engine.ts

--- game_engine.ts
+++ game_engine.ts
@@ -1,6 +1,11 @@
 class GameEngine {
   let instance: GameEngine = null;
   
+  function GameEngine() {
+    let game_div = document.createElement("div");
+    game_div.setAttribute("id", "gameSection");
+  }
+  
   function getInstance(): GameEngine {
     if(instance === null){
       instance = new GameEngine();
@@ -9,7 +14,6 @@ class GameEngine {
   }
   
   function init_game(game_index: number) {
-    let game_div = document.createElement("div");
-    game_div.setAttribute("id", "gameSection");
+    
   }
 }

da35412880f0e848b6e79482f7ee4ba2a1aeada2
Esteban JEAN <esteban-jean.pro@hotmail.com>
2023-08-14 15:48:20 +0200

    Language selector + footer credits + init_select

--- lang_settings.ts
+++ lang_settings.ts
@@ -4,43 +4,55 @@ class LanguageSettings {
   static readonly languages: string[] = [
     "English",
     "French"
-    ];
+  ];
+  
+  private static lang_selector : HTMLElement = document.getElementById("lang_selector")!;
 
-    static get lang(): string {
-      if(this.selected_lang > this.languages.length)
-        return this.languages[0];
-      return this.languages[this.selected_lang];
+  static init_select() {
+    for (let lang in LanguageSettings.languages) {
+      let option = document.createElement("option");
+      option.setAttribute("value", lang.toLowerCase());
+      //option.setAttribute("style", "");
+      option.innerText = lang.slice(0, 2).toUpperCase();
+      LanguageSettings.lang_selector.appendChild(option);
     }
+  }
 
-    static set select_lang(value : number) {
-      LanguageSettings.selected_lang = value;
-    }
+  static get lang(): string {
+    if(this.selected_lang > this.languages.length)
+      return this.languages[0];
+    return this.languages[this.selected_lang];
+  }
+
+  static set select_lang(value : number) {
+    LanguageSettings.selected_lang = value;
+  }
+  
+  static get_property_value(key : string, lang? : number | string) : string {
+    let query : string = './';
     
-    static get_property_value(key : string, lang? : number | string) : string {
-      let query : string = './';
+    switch (typeof lang) {
+      case 'number':
+        query += LanguageSettings.languages[lang];
+        break;
       
-      switch (typeof lang) {
-        case 'number':
-          query += LanguageSettings.languages[lang];
-          break;
-        
-        case "string":
-          query += lang;
-          break;
-        
-        default:
-          query += LanguageSettings.lang.toLowerCase();
-          break;
-      }
-
-      query += '.json';
-
-      let data = fetch(query, {
-        headers: {
-          'Accept': 'application/json'
-        }
-      }).then((res) => {return res.json()});
-
-      return data[key];
+      case "string":
+        query += lang;
+        break;
+      
+      default:
+        query += LanguageSettings.lang.toLowerCase();
+        break;
     }
+
+    query += '.json';
+
+    let data = fetch(query, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then((res) => {return res.json()});
+
+    return data[key];
+  }
 }
